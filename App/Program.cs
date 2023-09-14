9813fd08933b37a4ae5c87a574369dee2fd2ec25
wiiktaru <taruwiik@gmail.com>
2023-09-14 13:17:19 +0300

    initial commit2

---
+++ App/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AppLogger;
+using Humanizer;
+
+namespace App;
+
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            Logger.Log("Erkki Esimerkki"); 
+
+            string text = "Erkillä _oli _esimerkki_";
+            Console.WriteLine(text.Humanize()); 
+        }
+    }
+
+
+
+
+
+
